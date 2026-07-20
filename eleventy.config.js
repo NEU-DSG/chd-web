f90c1bf0228e95c703371715fa39d2c0ae8a9fdd
joelsjlee <joelsjlee@gmail.com>
2026-07-20 15:44:52 -0400

    add comments to eleventy config filters

--- eleventy.config.js
+++ eleventy.config.js
@@ -6,15 +6,18 @@ export default function (eleventyConfig) {
     errorMode: 'allow-fallback' // Opting out of "strict"
   });
 
+  // Get the last segment of a URL path.
   eleventyConfig.addFilter("lastSegment", url => {
   return url.split("/").filter(Boolean).pop();
   });
 
+  // Remove the domain portion of a URL.
   eleventyConfig.addFilter("firstSlash", url => {
   return url.split(".edu/")[1];
 });
 
 
+  // Convert singular property names to plural forms.
   eleventyConfig.addFilter("makePlural", prop => {
     if (prop == "founder") {
       return prop + "s"
@@ -23,6 +26,7 @@ export default function (eleventyConfig) {
     }
   });
 
+  // Convert plural property names back to singular forms.
   eleventyConfig.addFilter("makeSingular", prop => {
   if (prop === "founders") {
     return "founder";
@@ -33,20 +37,25 @@ export default function (eleventyConfig) {
   }
 });
 
+  // Split a string using a given separator.
 eleventyConfig.addFilter("split", (str, sep) => str.split(sep));
 
+  // Convert text into a URL-friendly format.
 eleventyConfig.addFilter("translate", (str) => {
   return str.replace(/ /g, "_").replace(/[().,\[\]"'<>{}|\\^]/g, "");
 });
 
+  // Swap the locale prefix in a URL.
   eleventyConfig.addFilter("locale_swap", function(url, targetLocale) {
   return url.replace(/^\/(en|zh-Hans|zh-Hant)\//, `/${targetLocale}/`);
 });
 
+  // Create a readable label from a URL.
   eleventyConfig.addFilter("labelFromUrl", (url) => {
   return url.split("/").pop().replaceAll("_", " ");
 });
 
+  // Convert strings to title case formatting.
   eleventyConfig.addFilter("changeCase", (str) => {
   return str
     .replace(/_list$/, "")
@@ -55,14 +64,17 @@ eleventyConfig.addFilter("translate", (str) => {
     .replace(/\b\w/g, (c) => c.toUpperCase());
 });
 
+  // Log values for debugging.
   eleventyConfig.addFilter('log', value => {
     console.log(value)
   });
 
+  // Capitalize words in a string.
   eleventyConfig.addFilter("capitalize", function(value) {
     return value.replace(/\b\w/g, char => char.toUpperCase());
 });
 
+  // Convert latitude/longitude strings into arrays.
   eleventyConfig.addFilter("toLatLng", (str) => {
     if (!str) return null;
     const [lat, lng] = str.split(",").map(Number);
@@ -71,10 +83,6 @@ eleventyConfig.addFilter("translate", (str) => {
 
   eleventyConfig.addPassthroughCopy('src/favicon.ico');
   eleventyConfig.addPassthroughCopy("src/styles");
-
-  ['src/favicon.ico'].forEach(item =>
-    eleventyConfig.addPassthroughCopy(item)
-  );
 
   return {
     dir: {
