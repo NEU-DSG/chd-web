e1872015d5b6ceee97ec7045b540b884e7a432a6
joelsjlee <joelsjlee@gmail.com>
2026-07-21 13:08:20 -0400

    edit open image graph to comment it out because we don't have one right now

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,5 +1,5 @@
 export const url = process.env.URL || 'http://localhost:8080';
-export const siteName = '18n-starter';
+export const siteName = 'Chinatown History Directory';
 export const siteDescription =
-  "Minimal starter for localized content, using Eleventy's own Internationalization (I18n) plugin";
-export const ogImage = '/opengraph.jpg';
+  "An index of sources on Boston's Chinatown";
+// export const ogImage = '/opengraph.jpg';
